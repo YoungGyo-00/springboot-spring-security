473ae23c49c6e3f5376df32f8eb3520f853d537c
younggyo <lee4519698@hanmail.net>
2023-01-29 01:07:49 +0800

    :sparkles: Feat : 구글 회원 프로필 정보 받아보기

--- 인프런 강의/src/main/java/com/cos/security1/config/SecurityConfig.java
+++ 인프런 강의/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.security1.config;
 
+import com.cos.security1.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -12,6 +14,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize 어노테이션 활성화
 public class SecurityConfig {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
     @Bean
@@ -35,7 +40,12 @@ public class SecurityConfig {
                 .defaultSuccessUrl("/")
                 .and()
                 .oauth2Login()
-                .loginPage("/loginForm"); // 구글 로그인이 완료된 뒤의 후처리가 필요함
+                .loginPage("/loginForm")
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
+
+        // 구글 로그인이 완료된 뒤의 후처리가 필요함. Tip 코드 x, (엑세스토큰 + 사용자프로팔정보 o)
+        // 1. 인증 코드 받기 2. 엑세스토큰(권한), 3. 사용자프로필 정보 가져오기, 4. 정보를 토대로 회원가입
 
         return http.build();
     }
