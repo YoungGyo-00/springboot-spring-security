1d7bd36fc9b9a672957824d435a05d27ab1b6c42
younggyo <lee4519698@hanmail.net>
2023-02-25 04:31:12 +0900

    :sparkles: JWT 로그인 필터 추가

--- jwt/src/main/java/com/example/jwt/config/SecurityConfig.java
+++ jwt/src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.example.jwt.config;
 
 import com.example.jwt.filter.MyFilter1;
+import com.example.jwt.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,6 +21,8 @@ public class SecurityConfig{
 
     private final CorsFilter corsFilter;
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.addFilterBefore(new MyFilter1(), SecurityContextPersistenceFilter.class);
@@ -27,6 +32,7 @@ public class SecurityConfig{
                 .addFilter(corsFilter) // @CorssOrigin(인증 x), 시큐리티 필터에 인증
                 .formLogin().disable()
                 .httpBasic().disable()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager(authenticationConfiguration))) // AuthenticationManager
                 .authorizeHttpRequests()
                 .requestMatchers("/api/vi/user/**")
                 .hasAnyAuthority("ROLE_USER", "ROLE_MANAGER", "ROLE_ADMIN")
@@ -37,5 +43,11 @@ public class SecurityConfig{
                 .anyRequest().permitAll();
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
---
+++ jwt/src/main/java/com/example/jwt/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package com.example.jwt.jwt;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+// 스프링 시큐리티에서 UsernamePasswordAuthenticationFilter
+// login 요청해서 username, password 전송하면
+// UsernamePasswordAuthenticationFilter 동작을 함
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+
+    // /login 요청을 하면 로그인 시도를 위해서 실행되는 함수
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        System.out.println("JWT 로그인 시도중");
+        return super.attemptAuthentication(request, response);
+    }
+}
